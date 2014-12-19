69ca89bc37368d3091a69e29505892a3f24e1636
letherman255 <matboularot@yahoo.fr>
2014-12-19 18:30:48 +0100

    added java system

---
+++ piWorkspace/GAM/src/Main/multemediaAccessManager.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class multemediaAccessManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		while (true) {
+System.out.println("test");
+		}
+
+	}
+
+}
