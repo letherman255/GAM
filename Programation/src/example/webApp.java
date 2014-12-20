ab2eb1f6e0166c460d4cc5950e354d6f3a2b4b9b
letherman255 <matboularot@yahoo.fr>
2014-12-20 17:32:24 +0100

    some work

--- Programation/src/example/webApp.java
+++ Programation/src/example/webApp.java
@@ -2,6 +2,7 @@ package example;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,18 +19,14 @@ import com.pi4j.io.gpio.RaspiPin;
 
 public class WebApp extends HttpServlet
 {
-    final GpioController gpio = GpioFactory.getInstance();
-
-    GpioPinDigitalInput boutton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, "boutton", PinPullResistance.PULL_DOWN);
-
-    boolean bouttonpressed = boutton.isHigh();
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
 
-        resp.getWriter().println(new Date());
-        resp.getWriter().println("test");
+        Random rand = new Random();
+        int rn = rand.nextInt(250);
+        resp.getWriter().println(rn);
     }
 }
