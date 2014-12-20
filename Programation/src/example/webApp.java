049015348f75b56a0a68fdc7f1d53e640590238d
letherman255 <matboularot@yahoo.fr>
2014-12-20 17:11:38 +0100

    gpio libs

--- Programation/src/example/webApp.java
+++ Programation/src/example/webApp.java
@@ -8,11 +8,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class webApp extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPin;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
 
-		resp.getWriter().println(new Date());
-	}
+public class WebApp extends HttpServlet
+{
+    final GpioController gpio = GpioFactory.getInstance();
+
+    GpioPinDigitalInput boutton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, "boutton", PinPullResistance.PULL_DOWN);
+
+    boolean bouttonpressed = boutton.isHigh();
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+
+        resp.getWriter().println(new Date());
+        resp.getWriter().println("test");
+    }
 }
