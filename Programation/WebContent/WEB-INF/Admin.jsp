1f03b524712cdbeb856fa533ab5a69ae046042dd
letherman255 <matboularot@yahoo.fr>
2014-12-26 18:25:48 +0100

    worked on website !
    +added proper html
    +added css
    +renamed Test.jsp -> admin.jsp

---
+++ Programation/WebContent/WEB-INF/Admin.jsp
@@ -0,0 +1,78 @@
+<%@page import="com.mcnanotech.beans.Coyote"%>
+<%@ page pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="ressources/style.css">
+<title>Gestionnaire d'accès Multimedia</title>
+</head>
+<body>
+	<div id="bloc_page">
+		<header> 
+			<img src="ressources/images/bannieres/banner_admin.jpg" alt="banner.jpg"/> 
+		</header>
+		<nav>
+			<ul>
+				<li><a href="#">Gestion utilisateurs</a></li>
+				<li><a href="#">configuration système</a></li>
+				<li><a href="#">Aide</a></li>
+			</ul>
+		</nav>
+		<hr>
+		<div class="infsys">
+			<h1>Info Système</h1> 
+		
+		</div>
+		<article>
+		<br>
+		</article>
+		
+		<section id="cx">
+		<div id="c1">
+			<br><br><br><br><br><br>
+			Je suis la console numérot 1 !
+		</div>
+		
+		<div id="c2">
+		<br><br><br><br><br><br>
+			Je suis la console numérot 2 !
+		</div>
+		
+		<div id="c3">
+		<br><br><br><br><br><br>
+			Je suis la console numérot 3 !
+		</div>
+		
+		<div id="c4">
+		<br><br><br><br><br><br>
+			Je suis la console numérot 4 !
+		</div>
+		</section>
+		
+		
+		<footer>
+		
+		</footer>
+		
+	</div>
+	<p>Ceci est une page générée depuis une JSP.</p>
+	<p>
+		<%
+		    String attribut = (String)request.getAttribute("test");
+		    out.println(attribut);
+
+		    String parametre = request.getParameter("auteur");
+		    out.println(parametre);
+		%>
+	</p>
+	<p>
+		Récupération du bean :
+		<%
+		Coyote notreBean = (Coyote)request.getAttribute("coyote");
+	    out.println(notreBean.getPrenom());
+	    out.println(notreBean.getNom());
+	%>
+	</p>
+</body>
+</html>
--- Programation/src/com/mcnanotech/servlets/Test.java
+++ Programation/src/com/mcnanotech/servlets/Test.java
@@ -38,75 +38,75 @@ public class Test extends HttpServlet
         request.setAttribute("coyote", premierBean);
 
         /* Transmission de la paire d'objets request/response à notre JSP */
-        this.getServletContext().getRequestDispatcher("/WEB-INF/Test.jsp").forward(request, response);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/Admin.jsp").forward(request, response);
 
-        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
-
-        // create gpio controller
-        final GpioController gpio = GpioFactory.getInstance();
-
-        // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
-        System.out.println("--> GPIO state should be: ON");
-
-        try
-        {
-            Thread.sleep(5000);
-        }
-        catch(InterruptedException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        // turn off gpio pin #01
-        pin.low();
-        System.out.println("--> GPIO state should be: OFF");
-
-        try
-        {
-            Thread.sleep(5000);
-        }
-        catch(InterruptedException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        // toggle the current state of gpio pin #01 (should turn on)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: ON");
-
-        try
-        {
-            Thread.sleep(5000);
-        }
-        catch(InterruptedException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        // toggle the current state of gpio pin #01 (should turn off)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: OFF");
-
-        try
-        {
-            Thread.sleep(5000);
-        }
-        catch(InterruptedException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        // turn on gpio pin #01 for 1 second and then off
-        System.out.println("--> GPIO state should be: ON for only 1 second");
-        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
-
-        // stop all GPIO activity/threads by shutting down the GPIO controller
-        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        gpio.shutdown();
+//        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
+//
+//        // create gpio controller
+//        final GpioController gpio = GpioFactory.getInstance();
+//
+//        // provision gpio pin #01 as an output pin and turn on
+//        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+//        System.out.println("--> GPIO state should be: ON");
+//
+//        try
+//        {
+//            Thread.sleep(5000);
+//        }
+//        catch(InterruptedException e)
+//        {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//
+//        // turn off gpio pin #01
+//        pin.low();
+//        System.out.println("--> GPIO state should be: OFF");
+//
+//        try
+//        {
+//            Thread.sleep(5000);
+//        }
+//        catch(InterruptedException e)
+//        {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//
+//        // toggle the current state of gpio pin #01 (should turn on)
+//        pin.toggle();
+//        System.out.println("--> GPIO state should be: ON");
+//
+//        try
+//        {
+//            Thread.sleep(5000);
+//        }
+//        catch(InterruptedException e)
+//        {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//
+//        // toggle the current state of gpio pin #01 (should turn off)
+//        pin.toggle();
+//        System.out.println("--> GPIO state should be: OFF");
+//
+//        try
+//        {
+//            Thread.sleep(5000);
+//        }
+//        catch(InterruptedException e)
+//        {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//
+//        // turn on gpio pin #01 for 1 second and then off
+//        System.out.println("--> GPIO state should be: ON for only 1 second");
+//        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
+//
+//        // stop all GPIO activity/threads by shutting down the GPIO controller
+//        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+//        gpio.shutdown();
     }
 }
