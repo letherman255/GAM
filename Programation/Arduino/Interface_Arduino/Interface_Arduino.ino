df623fb9e0b3b8e1dfb1eaf6a8b4258378214cd3
Eytraze <victorien.bauer@gmail.com>
2015-03-22 11:11:31 +0100

    Programme pour Arduino + Typon pour Alim (non fini)

---
+++ Programation/Arduino/Interface_Arduino/Interface_Arduino.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+
+int i2c;
+int M1 = 2; //Manette 1
+int M2 = 3; //Manette 2
+int M3 = 13; //Manette 3
+int M4 = 5; //Manette 4
+
+void setup(){
+  Serial.begin(9600); //Pour le debug
+  Wire.begin(0x10); //Adresse à changer pour chaque Arduino
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(M3, OUTPUT);
+  pinMode(M4, OUTPUT);
+  Serial.setTimeout(2);
+}
+
+void loop(){
+  i2c = Serial.parseInt();
+  if (i2c <= 0){
+    //Ne fais rien du tout !
+  }else{
+    switch(i2c){
+      case 10 :
+        digitalWrite(M1, LOW);
+        Serial.println("Manette n 1 eteinte !");
+        break;
+      case 11 :
+        digitalWrite(M1, HIGH);
+        Serial.println("Manette n 1 allumee !");
+        break;
+      case 20 :
+        digitalWrite(M2, LOW);
+        Serial.println("Manette n 2 eteinte !");
+        break;
+      case 21 :
+        digitalWrite(M2,HIGH);
+        Serial.println("Manette n 2 allumee !");
+        break;
+      case 30 :
+        digitalWrite(M3, LOW);
+        Serial.println("Manette n 3 eteinte !");
+        break;
+      case 31 :
+        digitalWrite(M3, HIGH);
+        Serial.println("Manette n 3 allumee !");
+        break;
+      case 40 :
+        digitalWrite(M4, LOW);
+        Serial.println("Manette n 4 eteinte !");
+        break;
+      case 41 :
+        digitalWrite(M4, HIGH);
+        Serial.println("Manette n 4 allumee !");
+        break;
+    }
+  }
+  delay(10);
+}
---
+++ sh+®ma/Alimentation.pro
@@ -0,0 +1,25 @@
+EAGLE AutoRouter Statistics:
+
+Job           : F:/Alimentation.brd
+
+Start at      : 16:20:11 (12/03/2015)
+End at        : 16:20:21 (12/03/2015)
+Elapsed time  : 00:00:01
+
+Signals       :     6   RoutingGrid: 25 mil  Layers: 1
+Connections   :    17   predefined:  0 ( 0 Vias )
+
+Router memory :   146608
+
+Passname          :    Busses     Route Optimize1 Optimize2 Optimize3 Optimize4
+
+Time per pass     :  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:01
+Number of Ripups  :         0         1         0         0         0         0
+max. Level        :         0         1         0         0         0         0
+max. Total        :         0         3         0         0         0         0
+
+Routed            :         3        16        16        16        16        16
+Vias              :         0         0         0         0         0         0
+Resolution        :    17.6 %    94.1 %    94.1 %    94.1 %    94.1 %    94.1 %
+
+Final             : 94.1% finished
